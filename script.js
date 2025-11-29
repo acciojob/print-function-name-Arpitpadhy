9219caa397c436280d9b6a4b2c88723fbb0346ea
Arpitpadhy <127716002+Arpitpadhy@users.noreply.github.com>
2025-11-29 19:22:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function functionName() {
+    // Using arguments.callee.name (works in non-strict mode)
+    alert(arguments.callee.name);
+}
+
+// Example: calling the function
+functionName();
